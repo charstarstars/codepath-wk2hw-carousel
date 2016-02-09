ef839bac5574836b7eadf456d7bede7d2462cd44
Ariel Liu <aliu@lyft.com>
2016-02-08 17:07:25 -0800

    Initial Commit

---
+++ codepath_wk2hw_carouselTests/codepath_wk2hw_carouselTests.swift
@@ -0,0 +1,36 @@
+//
+//  codepath_wk2hw_carouselTests.swift
+//  codepath_wk2hw_carouselTests
+//
+//  Created by Ariel Liu on 2/8/16.
+//  Copyright © 2016 Ariel Liu. All rights reserved.
+//
+
+import XCTest
+@testable import codepath_wk2hw_carousel
+
+class codepath_wk2hw_carouselTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
